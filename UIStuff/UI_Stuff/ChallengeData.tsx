4102d1eb14c79ee9c1e18afc4422bce1c5aa5faf
vicim994 <72468847+vicim994@users.noreply.github.com>
2022-03-10 11:49:45 +0100

    Dag3 scrum 2
    Bra dag. Spela in en löptur på god väg att bli klart.

---
+++ UIStuff/UI_Stuff/ChallengeData.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+var type = "distance";
+var distance_goal = 1000;
+var elapsed_distance = 0;
+var time = "null";
+var completed = false;
+
+export function getType(){
+
+    return type;
+}
+
+export function getTime() {
+
+    return time;
+}
+
+export function getDistanceGoal() {
+
+    return distance_goal;
+}
+
+export function checkCompleted(){
+
+    return completed;
+}
+
+export function setDistanceGoal(goal: any){
+    distance_goal = goal;
+}
+
+export function setElapsedDistance(dist: any){
+    elapsed_distance = dist;
+}
+
+export function setTime(t: any){
+    time = t;
+}
+
+export function CompleteChallenge(){
+    completed = true;
+}
+
--- UIStuff/UI_Stuff/screens/GPSPage.tsx
+++ UIStuff/UI_Stuff/screens/GPSPage.tsx
@@ -1,35 +1,17 @@
 import styles from '../styles/Page.style';
-
-//import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { Button, PermissionsAndroid } from "react-native";
 import React, { useState, useEffect } from 'react';
-import { render } from "react-dom";
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
-import { LocationSubscriber } from 'expo-location/build/LocationSubscribers';
-import { subscribeToPermissionUpdates } from 'react-native-location';
+import { Text, View } from '../components/Themed';
+import Recorder from '../components/Recorder';
 
 export default function GPSPage() {
     
-    const [distance, setDistance] = useState(0);
     const [position, setPosition] = useState([0,0]);
-    const [errorMsg, setErrorMsg] = useState(null);
+    const [errorMsg, setErrorMsg] = useState("null");
     
-    const haversine = require('haversine')
     let subscriber = null;
-
-    let challenge = {
-      distance: 1000,
-      completed: false
-    }
-
-    let run = {
-      distance: 0,
-      time: 0,
-      speed: 0,
-    }
-
+    
     useEffect(() => {
       (async () => {
         let { status } = await Location.requestForegroundPermissionsAsync();
@@ -41,7 +23,7 @@ export default function GPSPage() {
         subscriber = await Location.watchPositionAsync(
           {
               accuracy: Location.Accuracy.BestForNavigation,
-              distanceInterval: 20,
+              distanceInterval: 10,
           },
           (location) => {
               setPosition([location.coords.longitude, location.coords.latitude])
@@ -50,19 +32,11 @@ export default function GPSPage() {
       })()
     }, [])
 
-    useEffect(() => {
-      setDistance(distance+20);
-      console.log(distance);
-    }, [position])
-
     return (
-        <View style={styles.container}>
-            <View>
-            <Text>{position[0] + ", " + position[1]}</Text>
-                
-            </View>    
-        </View>
-    );    
+      <View style={styles.container}>
+        <Recorder position={position}></Recorder>  
+      </View>
+    );
 }
 
 //<Button title='Start' onPress={}/>

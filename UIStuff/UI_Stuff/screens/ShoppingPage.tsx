86e6001482e30f4e3b5e9c7bcd2d90c06fc8d2de
Sofia Sproge <sofsp932@student.liu.se>
2022-03-28 09:37:24 +0200

    Shop update
    Lagt till bilder (character), fixat shopping page och lite style

--- UIStuff/UI_Stuff/screens/ShoppingPage.tsx
+++ UIStuff/UI_Stuff/screens/ShoppingPage.tsx
@@ -1,14 +1,88 @@
 import styles from '../styles/Page.style';
-
+import shopStyles from '../styles/shop.style';
 //import EditScreenInfo from '../components/EditScreenInfo';
+import React, { useState } from 'react';
 import { Text, View } from '../components/Themed';
+import { Image, ImageBackground, Pressable} from "react-native";
+
 
 export default function ShoppingPageScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Shopping Page</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {/*<EditScreenInfo path="/screens/ShoppingPage.tsx" >*/}
-    </View>
-  );
-}
+ 
+  const [headImg, setHeadImg] = useState (require('../Graphics/character/kepps.png'));
+  const [shirtImg,setShirtImg] = useState (require('../Graphics/character/hoodie.png'));
+ 
+   return (
+     <View style={styles.container}>
+       <ImageBackground source={require('../Graphics/forest.png')} style={styles.absolute} resizeMode="cover">
+       <ImageBackground source={require('../Graphics/banan.png')} style={styles.banan} resizeMode="stretch">
+       
+       
+       {/*<Image source={require('../Graphics/forest.png')} style={styles.absolute} resizeMode="cover"></Image>*/}
+       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+       
+       <View style = {shopStyles.characterContainer}>
+         {/*Original character is always rendered */}
+         <ImageBackground
+           source={require('../Graphics/character/wut2.png')}
+           style={shopStyles.character} resizeMode="cover">
+         </ImageBackground>
+       
+         <ImageBackground
+           source={headImg}
+           style={shopStyles.character} resizeMode="cover">
+         </ImageBackground>
+ 
+         <ImageBackground
+           source={shirtImg}
+           style={shopStyles.character} resizeMode="cover">
+         </ImageBackground>
+         
+       </View>
+ 
+       <View style = {shopStyles.shopMenu}>
+       
+       <Text></Text>
+         <Pressable
+           onPress={() => setHeadImg(require('../Graphics/character/kepps2.png'))}
+           style={shopStyles.shopButton}>
+           <Text>Set Moose hat</Text>
+         </Pressable>
+         
+         <Pressable
+           onPress={() => setShirtImg(require('../Graphics/character/hoodie2.png'))}
+           style={shopStyles.shopButton}>
+                
+           <Text>Set noise Hoodie</Text>
+         </Pressable>
+ 
+         <Pressable
+            onPress={() => setHeadImg(require('../Graphics/character/kepps.png'))}
+            style={shopStyles.shopButton}>
+           <Text>Set Blue hat</Text>
+         </Pressable>
+        
+         <Pressable
+           onPress={() => setShirtImg(require('../Graphics/character/hoodie.png'))}
+           style={shopStyles.shopButton}>
+           <Text>Set blue hoodie</Text>
+         </Pressable>
+ 
+         <Pressable
+           onPress={() => setHeadImg(require('../Graphics/character/empty.png'))}
+           style={shopStyles.shopButton}>
+           <Text>Set original hair</Text>
+         </Pressable>
+         
+         <Pressable
+           onPress={() => setShirtImg(require('../Graphics/character/empty.png'))}
+           style={shopStyles.shopButton}>
+           <Text>Set original shirt</Text>
+         </Pressable>
+         
+       </View>
+ 
+       </ImageBackground>
+       </ImageBackground>
+     </View>
+   );
+ }
---
+++ UIStuff/UI_Stuff/styles/shop.style.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native';
+
+{/*StyleSheet for the base of the pages*/}
+export default StyleSheet.create({
+    
+
+    //Shop page style
+
+    //Container that holds every item on the character
+    characterContainer:{
+      justifyContent: 'center',
+      position:'relative',
+      alignItems: 'center',
+      backgroundColor: 'rgba(52, 52, 52, 0)',
+      top: '-18%',
+    },
+
+    //Used by every character item
+    character:{
+      flex: 1,
+      height: 250,
+      width: 250,
+      position: 'absolute',
+      scaleX: "-1",
+      zIndex: -1,
+    },
+
+    // Holds the shop information
+    shopMenu:{
+      top:'7%',
+      height:'32%',
+      width: '90%',
+      backgroundColor: 'rgba(52, 52, 52, 0)',
+      flexDirection:"row",
+      flexWrap:"wrap",
+      justifyContent: 'center',
+      alignItems:'center',
+    },
+    
+    //Evvery button in the shop
+    shopButton:{
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+      backgroundColor: 'pink',
+      alignSelf: 'flex-start',
+      marginHorizontal:"2%",
+      marginVertical:"2%",
+      textAlign:'center',
+      borderRadius: 5,
+    }
+
+  });

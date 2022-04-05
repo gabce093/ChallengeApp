e70a27416653130a0bcbc158ba3c294496289ed8
vicim994 <72468847+vicim994@users.noreply.github.com>
2022-04-05 08:21:22 +0200

    lalal

--- UIStuff/UI_Stuff/screens/MainPage.tsx
+++ UIStuff/UI_Stuff/screens/MainPage.tsx
@@ -18,26 +18,15 @@ import { RootTabScreenProps, RootStackParamList, RootTabParamList, RootStackScre
 
 export default function MainPageScreen({ navigation }: RootStackScreenProps<'CreateRunPage'>) {
  
- 
+ //<WeeklyChallengeOrb percent={50}/>
   return (
     <View style={styles.topContainer}>
       
       <ImageBackground source={require('../Graphics/forest.png')} style={styles.absolute} resizeMode="cover">
       <ImageBackground source={require('../Graphics/banan.png')} style={styles.banan} resizeMode="stretch">
       
-      <LoginPage/>
-      <View style={styles.playerContainer}>
+     
       
-      <View style={styles.subContainer}>
-        <WeeklyChallengeOrb percent={50}/>
-          <Text style={styles.username}>theSonfofaasd</Text>
-        </View>
-        
-        <View style={styles.subContainer}>
-          <ImageBackground source={require('../Graphics/wut2w_kepps.png')} style={styles.playerImage} resizeMode="cover"></ImageBackground>
-        </View>
-      </View>
-
       {/*<Image source={require('../Graphics/forest.png')} style={styles.absolute} resizeMode="cover"></Image>*/}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       
@@ -45,7 +34,8 @@ export default function MainPageScreen({ navigation }: RootStackScreenProps<'Cre
       <Pressable style={styles.challengeButton}
         onPress={() => navigation.navigate('CreateRunPage')}>
       <Text style={styles.challengeText}>Challenge!</Text>
-      </Pressable>
+      </Pressable> 
+      <LoginPage/>
       </ImageBackground>
       </ImageBackground>
     </View>

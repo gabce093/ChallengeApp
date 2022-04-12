d71574bacfc0254e6d97e6d82b9a4e2ccb32f5d3
Henrik <72468904+Hengu510@users.noreply.github.com>
2022-04-12 08:48:47 +0200

    TopBarFix

--- UIStuff/UI_Stuff/screens/StatPage.tsx
+++ UIStuff/UI_Stuff/screens/StatPage.tsx
@@ -1,14 +1,43 @@
-import styles from '../styles/Page.style';
+import pageStyles from '../styles/Page.style';
+import statPageStyles from '../styles/StatPage.style';
+import { ImageBackground} from "react-native";
 
 //import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function StatPageScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Stat Page</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {/*<EditScreenInfo path="/screens/StatPage.tsx" >*/}
-    </View>
+    <ImageBackground source={require('../Graphics/forest.png')} style={pageStyles.forestBackground} resizeMode="cover">
+      <View style={statPageStyles.MainContainer}>
+          <Text style={statPageStyles.title}>Stat Page</Text>
+            <View style={statPageStyles.statHolder}>
+
+              <View style={statPageStyles.row}>
+                <Text style={statPageStyles.statText}>Battles Won:</Text>
+                <Text style={statPageStyles.statText}>null</Text>
+              </View>
+
+              <View style={statPageStyles.row}>
+                <Text style={statPageStyles.statText}>Avrage runtime:</Text>
+                <Text style={statPageStyles.statText}>null</Text>
+              </View>
+
+              <View style={statPageStyles.row}>
+                <Text style={statPageStyles.statText}>Avrage length:</Text>
+                <Text style={statPageStyles.statText}>null</Text>
+              </View>
+              
+              <View style={statPageStyles.row}>
+                <Text style={statPageStyles.statText}>Total kms:</Text>
+                <Text style={statPageStyles.statText}>null</Text>
+              </View>
+
+              <View style={statPageStyles.row}>
+                <Text style={statPageStyles.statText}>Total Steps:</Text>
+                <Text style={statPageStyles.statText}>null</Text>
+              </View>
+          </View>
+      </View>
+    </ImageBackground>
   );
 }

5091b9195ce3078697cea89d37f18a8444e8b55d
vicim994 <72468847+vicim994@users.noreply.github.com>
2022-05-10 09:26:55 +0200

    Update StatPage.style.js

--- UIStuff/UI_Stuff/styles/StatPage.style.js
+++ UIStuff/UI_Stuff/styles/StatPage.style.js
@@ -1,50 +1,92 @@
-import { StyleSheet, Dimensions } from 'react-native';
-var screenWidth = Dimensions.get('window').width;
+// import { flexbox } from 'native-base/lib/typescript/theme/styled-system';
+import { StyleSheet } from 'react-native';
 
-/**
- * This function contains the style of the base stat page.
- * @author Henrik Gustafsson
- */
+{/*StyleSheet for the base of the pages*/}
 export default StyleSheet.create({
-    MainContainer: {
-        position: "relative",
-        flex: 1,
-        margin: "5%",
-        marginTop: "20%",
-        backgroundColor: '#333333',
-        opacity: .8,
-        borderRadius: 20,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title:{
-        flex: 1,
-        flexDirection: "row",
-        fontSize: 0.11*screenWidth,
-        fontWeight: 'bold',
-        justifyContent: "center",
-        alignItems: "center",
+    
+    //Shop page style
 
+    //Container that holds every item on the character
+    characterContainer:{
+      justifyContent: 'center',
+      position:'relative',
+      alignItems: 'center',
+      backgroundColor: 'rgba(52, 52, 52, 0)',
+      top: '-18%',
     },
-    statHolder:{
-        flex: 8,
-        alignItems: "flex-start",
-        backgroundColor: "transparent", 
-        width: "100%",
-    },
-    row:{
-        flex: 1,     
-        backgroundColor: "rgba(0,0,0,0.5)", 
-        padding: "2%",
-        margin: "1%",
-        borderRadius: 15,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    statText:{
-        width: "50%",
-        fontSize: 0.05*screenWidth,
+
+    bgTrack: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        top: "10%",
+        
+      },
+    // progresText: {
+    //     backGroundColor: 'rgb(55, 55, 54)',
+    // },
+    infoDist: {
+        color: 'white',
         fontWeight: 'bold',
+        fontSize: 60,
     },
-});
+    descriptionText: {
+        color: 'white',
+        fontSize: 12,
+    },
+    largeInfoTextContainer: {
+        height: '10%',
+        width: '99%',
+        borderRadius:5,
+        marginTop: '15%',
+        marginLeft: '5%',
+     },
+
+    titleText:{
+      alignContent: 'center', 
+      fontSize: 45, 
+      fontWeight: 'bold',
+      marginBottom: 10,
+      marginTop:30,
+    },
+
+    // Holds the shop information
+    shopMenu:{
+      height: 200,
+      width: 350,
+      backgroundColor: 'rgba(52, 52, 52, 0)',
+      justifyContent: 'center',
+      alignItems:'center',
+    
+    },
+    
+    //Every button in the shop
+    shopButton:{
+      backgroundColor: '#939393',
+      margin: 10,
+      textAlign:'center',
+      borderRadius: 5,
+    },
+
+    prevImg:{
+      height: 90,
+      width: 90,
+      alignSelf: 'center',
+    },
+
+    itemText:{
+      borderRadius: 5,
+      backgroundColor: '#FF5C00',
+    },
+
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: '1%',
+      marginVertical: '1%',
+      marginHorizontal: '1%',
+      width: '50%',
+      borderRadius: 5,
+    },
+   
+  });

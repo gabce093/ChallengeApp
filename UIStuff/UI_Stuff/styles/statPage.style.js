cb2f2ad6479027874d8ac36b2ed20be6352875df
gabce093 <gabce093@student.liu.se>
2022-05-11 13:09:50 +0200

    Working now

--- UIStuff/UI_Stuff/styles/statPage.style.js
+++ UIStuff/UI_Stuff/styles/statPage.style.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 // var screenWidth = Dimensions.get('window').width;
 
 /**
@@ -6,101 +6,94 @@ import { StyleSheet } from 'react-native';
  * @author Fredrik Håkansson
  */
 
-
-
 export default StyleSheet.create({
-    
-    infoDist: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 60,
-    },
-    descriptionText: {
-        color: 'white',
-        fontSize: 12,
-    },
-    largeInfoTextContainer: {
-        height: '10%',
-        width: '99%',
-        borderRadius:5,
-        marginTop: '15%',
-        marginLeft: '5%',
-     },
-
-     challengeTitleText:{
-      alignContent: 'center', 
-      fontSize: '200%', 
-      fontWeight: 'bold',
-      marginBottom: '1%',
-      marginTop: '1%',
-      color: 'rgba(255, 255, 255, 1)',
-    },
-
-    
-    challengeItem: {
-      backgroundColor: 'rgba(134, 131, 131, 0.9)',
-      padding: '1%',
-      marginVertical: '1%',
-      marginHorizontal: '0%',
-      width: '49%',
-      borderRadius: 5,
-      marginRight: '2%',
-    },
-
-    ChallengeRow:{
-      flex: 1,     
-      // backgroundColor: 'rgba(134, 131, 131, 0.0)', 
-      padding: "2%",
-      margin: "0%",
-      borderRadius: 5,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "left",
+  infoDist: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 60,
   },
-    
-    itemText:{
-      borderRadius: 5,
-      fontSize: '100%', 
-      fontWeight: 'bold',
-      marginBottom: '1%',
-      marginTop: '1%',
-      color: 'rgba(255, 255, 255, 1)',
-    },
-
-    title:{
-      flex: 1,
-      flexDirection: "row",
-      fontSize: 5,
-      fontWeight: 'bold',
-      justifyContent: "center",
-      alignItems: "center",
-
+  descriptionText: {
+    color: "white",
+    fontSize: 12,
+  },
+  largeInfoTextContainer: {
+    height: "10%",
+    width: "99%",
+    borderRadius: 5,
+    marginTop: "15%",
+    marginLeft: "5%",
   },
 
-  statHolder:{
-      flex: 8,
-      alignItems: "flex-start",
-      backgroundColor: "transparent", 
-      width: "100%",
-      color: 'white',
-  },
-  row:{
-      flex: 1,     
-      backgroundColor: 'rgba(134, 131, 131, 0.9)', 
-      padding: "3%",
-      margin: "0%",
-      marginVertical: '1%',
-      borderRadius: 5,
-      flexDirection: "row",
-      justifyContent: "space-evenly",
-      alignItems: "center",
-  },
-  statText:{
-      width: "50%",
-      fontSize: 5,
-      fontWeight: 'bold',
-      color: 'white',
+  challengeTitleText: {
+    alignContent: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: "1%",
+    marginTop: "1%",
+    color: "rgba(255, 255, 255, 1)",
   },
 
-   
-  });
+  challengeItem: {
+    backgroundColor: "rgba(134, 131, 131, 0.9)",
+    padding: "1%",
+    marginVertical: "1%",
+    marginHorizontal: "0%",
+    width: "49%",
+    borderRadius: 5,
+    marginRight: "2%",
+  },
+
+  ChallengeRow: {
+    flex: 1,
+    // backgroundColor: 'rgba(134, 131, 131, 0.0)',
+    padding: "2%",
+    margin: "0%",
+    borderRadius: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+
+  itemText: {
+    borderRadius: 5,
+    fontSize: 12,
+    fontWeight: "bold",
+    marginBottom: "1%",
+    marginTop: "1%",
+    color: "rgba(255, 255, 255, 1)",
+  },
+
+  title: {
+    flex: 1,
+    flexDirection: "row",
+    fontSize: 5,
+    fontWeight: "bold",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  statHolder: {
+    flex: 8,
+    alignItems: "flex-start",
+    backgroundColor: "transparent",
+    width: "100%",
+    color: "white",
+  },
+  row: {
+    flex: 1,
+    backgroundColor: "rgba(134, 131, 131, 0.9)",
+    padding: "3%",
+    margin: "0%",
+    marginVertical: "1%",
+    borderRadius: 5,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  statText: {
+    width: "50%",
+    fontSize: 5,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
